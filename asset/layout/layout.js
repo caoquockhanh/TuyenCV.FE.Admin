55ac426e529bd7e9938959ba32a0e0616fa22237
caoquockhanh <khanhmin15@gmail.com>
2025-11-04 11:40:57 +0700

    update icon and display full name and email

--- asset/layout/layout.js
+++ asset/layout/layout.js
@@ -1,3 +1,4 @@
+// Hàm tải nội dung trang vào khu vực main-content
 function loadPage(page, element) {
   fetch(page)
     .then(res => res.text())
@@ -15,11 +16,55 @@ function loadPage(page, element) {
 
 // Lấy thông tin user và hiển thị tên lên giao diện
 function displayUserName() {
+
+  // Gọi API lấy thông tin người dùng
   getUserInfoAPI().then(res => {
     const userName = res.data.name;
-    document.querySelector('.name').textContent = userName;
+    const userEmail = res.data.email;
+    // Tách tên từ full name
+    const [firstName, ...rest] = userName.split(" ");
+    const middleName = rest.length === 1 ? "" : rest.slice(0, -1).join(" ");
+    const lastName = rest.at(-1);
+    const parseWorker = {firstName, middleName, lastName};
+    //console.log(parseWorker);
+    
+    // Hiển thị tên người dùng lên giao diện
+    document.querySelector('.name').textContent = parseWorker.lastName;
+
+    //Hiện thị full name trong modal info user
+    $('.info_name').text(userName);
+    //Hiện thị email trong modal info user
+    $('.info_email').text(userEmail);
   }).catch(err => {
     console.error('Lỗi khi lấy thông tin người dùng:', err);
+  });
+}
+
+// Xử lý sự nút down/up bên cạnh tên user
+function toggleChevron() {
+  const chevron = $('.info');
+  const chevronDown = $('.fa-chevron-down');
+  const chevronUp = $('.fa-chevron-up');
+  let isUp = false;
+  
+  chevron.click(() => {
+    if (!isUp) {
+      chevronDown.hide();
+      chevronUp.show();
+    } else {
+      chevronUp.hide();
+      chevronDown.show();
+    }
+    isUp = !isUp;
+  });
+};
+
+// Sự kiện click nút info user
+function modalInfoUser() {
+  const infoUser = $('.info');
+  const infoUserModal = $('.infoUser');
+  infoUser.click(function() {
+    infoUserModal.toggle();
   });
 }
 
@@ -29,4 +74,7 @@ window.onload = () => {
   displayUserName();
   const defaultTab = document.querySelector('#sidebar ul li');
   loadPage('../../pages/dashboardPage.html', defaultTab);
+  toggleChevron()
+  modalInfoUser()
+  
 };
